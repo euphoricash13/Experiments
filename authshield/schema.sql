4cdf05036e7e0eca3f4474e812c009d20191fbff
Ash Maddi <robinstrike8@gmail.com>
2025-01-31 15:33:16 -0600

    docs: add examples to README

--- authshield/schema.sql
+++ authshield/schema.sql
@@ -1,3 +1,4 @@
 "Updated on 2025-01-08T16:29:17.000Z\nProject: AuthShield\nFile: schema.sql" 
 "Updated on 2025-01-09T23:50:36.000Z\nProject: AuthShield\nFile: schema.sql" 
 "Updated on 2025-01-20T20:58:06.000Z\nProject: AuthShield\nFile: schema.sql" 
+"Updated on 2025-01-31T21:33:16.000Z\nProject: AuthShield\nFile: schema.sql" 
