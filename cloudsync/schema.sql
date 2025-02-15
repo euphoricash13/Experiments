d6310d03629eb656a03cd80c082127ffe69ce7e1
Ash Maddi <robinstrike8@gmail.com>
2025-02-15 10:15:12 -0600

    docs: add examples to README

--- cloudsync/schema.sql
+++ cloudsync/schema.sql
@@ -1,2 +1,3 @@
 "Updated on 2025-01-02T18:20:16.000Z\nProject: CloudSync\nFile: schema.sql" 
 "Updated on 2025-02-12T16:57:39.000Z\nProject: CloudSync\nFile: schema.sql" 
+"Updated on 2025-02-15T16:15:12.000Z\nProject: CloudSync\nFile: schema.sql" 
