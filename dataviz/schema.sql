7b6f022daad51d48def3ab5962c9e8e718421db8
Ash Maddi <robinstrike8@gmail.com>
2025-02-16 08:34:27 -0600

    fix: bug in DataViz logic

--- dataviz/schema.sql
+++ dataviz/schema.sql
@@ -1,2 +1,3 @@
 "Updated on 2025-01-09T15:58:25.000Z\nProject: DataViz\nFile: schema.sql" 
 "Updated on 2025-01-14T16:43:00.000Z\nProject: DataViz\nFile: schema.sql" 
+"Updated on 2025-02-16T14:34:27.000Z\nProject: DataViz\nFile: schema.sql" 
