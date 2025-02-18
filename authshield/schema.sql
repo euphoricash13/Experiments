7c77bee4f9b12b1d4961af7b98fb3165ef8a0d80
Ash Maddi <robinstrike8@gmail.com>
2025-02-18 11:05:51 -0600

    perf: optimize AuthShield performance

--- authshield/schema.sql
+++ authshield/schema.sql
@@ -4,3 +4,4 @@
 "Updated on 2025-01-31T21:33:16.000Z\nProject: AuthShield\nFile: schema.sql" 
 "Updated on 2025-02-13T17:31:06.000Z\nProject: AuthShield\nFile: schema.sql" 
 "Updated on 2025-02-14T16:58:35.000Z\nProject: AuthShield\nFile: schema.sql" 
+"Updated on 2025-02-18T17:05:51.000Z\nProject: AuthShield\nFile: schema.sql" 
