e31eae29f8c063fe08c47b277732ddd467c1a26d
Ash Maddi <robinstrike8@gmail.com>
2025-02-23 12:01:20 -0600

    Improve DataViz error handling

--- dataviz/schema.sql
+++ dataviz/schema.sql
@@ -2,3 +2,4 @@
 "Updated on 2025-01-14T16:43:00.000Z\nProject: DataViz\nFile: schema.sql" 
 "Updated on 2025-02-16T14:34:27.000Z\nProject: DataViz\nFile: schema.sql" 
 "Updated on 2025-02-20T18:57:29.000Z\nProject: DataViz\nFile: schema.sql" 
+"Updated on 2025-02-23T18:01:20.000Z\nProject: DataViz\nFile: schema.sql" 
