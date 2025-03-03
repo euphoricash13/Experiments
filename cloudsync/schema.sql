2d120eef9ea27428c0f792ccbb880da38b416e9b
Ash Maddi <robinstrike8@gmail.com>
2025-03-03 15:41:58 -0600

    style: format code

--- cloudsync/schema.sql
+++ cloudsync/schema.sql
@@ -1,3 +1,4 @@
 "Updated on 2025-01-02T18:20:16.000Z\nProject: CloudSync\nFile: schema.sql" 
 "Updated on 2025-02-12T16:57:39.000Z\nProject: CloudSync\nFile: schema.sql" 
 "Updated on 2025-02-15T16:15:12.000Z\nProject: CloudSync\nFile: schema.sql" 
+"Updated on 2025-03-03T21:41:58.000Z\nProject: CloudSync\nFile: schema.sql" 
