f7acaf86fc8e52676fb777f45b7a6d044f2d8fe2
Ash Maddi <robinstrike8@gmail.com>
2025-04-24 12:49:50 -0500

    fix: resolve issues in DataViz

--- dataviz/schema.sql
+++ dataviz/schema.sql
@@ -8,3 +8,4 @@
 "Updated on 2025-03-19T16:51:36.000Z\nProject: DataViz\nFile: schema.sql" 
 "Updated on 2025-04-08T15:29:29.000Z\nProject: DataViz\nFile: schema.sql" 
 "Updated on 2025-04-12T20:10:30.000Z\nProject: DataViz\nFile: schema.sql" 
+"Updated on 2025-04-24T17:49:50.000Z\nProject: DataViz\nFile: schema.sql" 
