a2833af642b783a99d39d92058ec0632082cb7db
Ash Maddi <robinstrike8@gmail.com>
2025-05-02 13:21:12 -0500

    docs: update APIGateway documentation

--- apigateway/schema.sql
+++ apigateway/schema.sql
@@ -1,3 +1,4 @@
 "Updated on 2025-02-01T17:04:20.000Z\nProject: APIGateway\nFile: schema.sql" 
 "Updated on 2025-02-20T23:30:10.000Z\nProject: APIGateway\nFile: schema.sql" 
 "Updated on 2025-04-10T14:40:15.000Z\nProject: APIGateway\nFile: schema.sql" 
+"Updated on 2025-05-02T18:21:12.000Z\nProject: APIGateway\nFile: schema.sql" 
