b75ec4ecc484894ba9bbcecf6f1434871b3bd06d
Ash Maddi <robinstrike8@gmail.com>
2025-05-30 16:33:20 -0500

    feat: add new features to CloudSync

--- cloudsync/schema.sql
+++ cloudsync/schema.sql
@@ -5,3 +5,4 @@
 "Updated on 2025-04-02T16:43:34.000Z\nProject: CloudSync\nFile: schema.sql" 
 "Updated on 2025-04-17T21:32:56.000Z\nProject: CloudSync\nFile: schema.sql" 
 "Updated on 2025-05-08T17:44:58.000Z\nProject: CloudSync\nFile: schema.sql" 
+"Updated on 2025-05-30T21:33:20.000Z\nProject: CloudSync\nFile: schema.sql" 
