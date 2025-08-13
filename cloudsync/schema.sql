1471cfc8c2beff6d9bbdad3963f896b27b8d2461
Ash Maddi <robinstrike8@gmail.com>
2025-08-13 13:17:43 -0500

    ci: update build configuration

--- cloudsync/schema.sql
+++ cloudsync/schema.sql
@@ -9,3 +9,4 @@
 "Updated on 2025-05-30T20:27:05.000Z\nProject: CloudSync\nFile: schema.sql" 
 "Updated on 2025-06-17T22:41:17.000Z\nProject: CloudSync\nFile: schema.sql" 
 "Updated on 2025-07-09T19:15:04.000Z\nProject: CloudSync\nFile: schema.sql" 
+"Updated on 2025-08-13T18:17:43.000Z\nProject: CloudSync\nFile: schema.sql" 
