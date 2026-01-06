9efed3bfc73f80b93a5c1910d26a39930475aa9c
Ash Maddi <robinstrike8@gmail.com>
2026-01-06 16:53:21 -0600

    perf: optimize TaskFlow performance

--- taskflow/schema.sql
+++ taskflow/schema.sql
@@ -20,3 +20,4 @@
 "Updated on 2025-12-08T22:30:41.000Z\nProject: TaskFlow\nFile: schema.sql" 
 "Updated on 2025-12-10T21:12:25.000Z\nProject: TaskFlow\nFile: schema.sql" 
 "Updated on 2025-12-17T21:55:39.000Z\nProject: TaskFlow\nFile: schema.sql" 
+"Updated on 2026-01-06T22:53:21.000Z\nProject: TaskFlow\nFile: schema.sql" 
